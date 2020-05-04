b72ff417ae720922240aa6155290bf069bece779
Sumin Kim <61191808+suminkimalff@users.noreply.github.com>
2020-05-04 16:12:36 +0200

    first commit

---
+++ WebApplication4/Models/Liste.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace WebApplication4.Models
+{
+    public class Liste
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Anzahl { get; set; }
+    }
+
+    public class ListeDBContext : DbContext
+    {
+        public DbSet<Liste> liste { get; set; }
+    }
+}
